5137530c556afafcb70798f0f2680672e7c07185
Andrei Berceanu <andreicberceanu@gmail.com>
2020-01-20 23:55:08 +0200

    Draft complete

--- iteration.py
+++ iteration.py
@@ -2,21 +2,21 @@ import numpy as np
 from numpy import linalg as LA
 from scipy import optimize
 
-# def powerIteration(transitionWeights, rsp=0.15, epsilon=0.00001, maxIterations=1000):
-#     # Power iteration:
-#     for iteration in range(maxIterations):
-#         oldState = state.copy()
-#         state = state.dot(transitionProbabilities)
-#         delta = state - oldState
-#         if __euclideanNorm(delta) < epsilon:
-#             break
-#     return state
-
-
 if __name__ == '__main__':
-    def func(x, c1, c2):
-        return np.sqrt(c1 / (x + c2))
 
-    c1 = np.array([10, 12.])
-    c2 = np.array([3, 5.])
-    o = optimize.fixed_point(func, [1.2, 1.3], args=(c1, c2))
+    def rhs(X, A, alpha):
+        return (1 - alpha) * X + alpha / np.sum(X) * np.dot(A, X)
+
+    m = 10  # number of agents
+    # A = np.identity(m)  # grade matrix, shape m x m
+    A = np.full((m, m), 0.8)
+    alpha = 0.1  # convergence parameter
+
+    X0 = np.dot(A, np.full(m, 1/m))
+    X = optimize.fixed_point(rhs, X0, args=(A, alpha))
+    print(X)
+
+    w, v = LA.eig(A)
+    # A.X = np.sum(X)*X
+
+
